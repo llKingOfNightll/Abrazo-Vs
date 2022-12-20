2ee2cb263d222d475927ec37c35acea51ae74f0a
andres <jeirex12mis@gmail.com>
2022-12-20 01:45:31 -0500

    mi primer commit

---
+++ script.js
@@ -0,0 +1,13 @@
+setTimeout(cambiaMensaje,2500)
+
+function cambiaMensaje() {
+    document.getElementById("loading-message").innerText = "Enviado!";
+  
+}
+
+setTimeout(function() {
+    alert("Recibiste un abrazo")
+}, 2300)
+
+
+
